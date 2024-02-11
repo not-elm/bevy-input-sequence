0eaad8e8b418bc7d9d8c95242ff60baa1443b08d
Shane Celis <shane.celis@gmail.com>
2024-02-11 11:58:06 -0500

    feature: Add winit example.

---
+++ macro/examples/winit.rs
@@ -0,0 +1,74 @@
+#![allow(clippy::single_match)]
+use std::collections::HashMap;
+#[cfg(any(target_os = "macos", target_os = "windows", target_os = "linux"))]
+use winit::{
+    dpi::LogicalSize,
+    event::{ElementState, Event, WindowEvent},
+    event_loop::EventLoop,
+    keyboard::{Key, ModifiersState, PhysicalKey, KeyCode},
+    // WARNING: This is not available on all platforms (for example on the web).
+    platform::modifier_supplement::KeyEventExtModifierSupplement,
+    window::{Window, WindowBuilder},
+};
+use keyseq_macro::{key, keyseq};
+
+#[cfg(not(any(target_os = "macos", target_os = "windows", target_os = "linux")))]
+fn main() {
+    println!("This example is not supported on this platform");
+}
+
+#[cfg(any(target_os = "macos", target_os = "windows", target_os = "linux"))]
+fn main() -> Result<(), impl std::error::Error> {
+    let event_loop = EventLoop::new().unwrap();
+
+    let window = WindowBuilder::new()
+        .with_inner_size(LogicalSize::new(400.0, 200.0))
+        .build(&event_loop)
+        .unwrap();
+
+    let mut modifiers = ModifiersState::default();
+    let mut binds = HashMap::new();
+    for (mods, key) in [ key! { 2 }, key! { shift-1 } ] {
+        binds.insert(key, mods);
+    }
+
+    event_loop.run(move |event, elwt| {
+        if let Event::WindowEvent { event, .. } = event {
+            match event {
+                WindowEvent::CloseRequested => elwt.exit(),
+                WindowEvent::ModifiersChanged(new) => {
+                    modifiers = new.state();
+                }
+                WindowEvent::KeyboardInput { event, .. } => {
+                    if event.state == ElementState::Pressed && !event.repeat {
+                        match event.key_without_modifiers().as_ref() {
+                            Key::Character("1") => {
+                                if modifiers.shift_key() {
+                                    println!("Shift + 1 | logical_key: {:?}", event.logical_key);
+                                } else {
+                                    println!("1");
+                                }
+                            }
+                            _ => (),
+                        }
+                        if let PhysicalKey::Code(key_code) = event.physical_key {
+                            if let Some(mods) = binds.get(&key_code) {
+                                if mods == &modifiers {
+                                    println!("Got key");
+                                } else {
+                                    println!("No key {:?}", modifiers);
+
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+                WindowEvent::RedrawRequested => {
+                }
+                _ => (),
+            }
+        };
+    })
+}
--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -122,7 +122,83 @@ fn modifiers_id(id: TokenStream, modifier: Modifier) -> TokenStream {
     q
 }
 
-fn get_key_code(tree: TokenTree) -> Option<Ident> {
+fn get_key_code_winit(tree: TokenTree) -> Option<Ident> {
+    match tree {
+        TokenTree::Literal(ref literal) => {
+            let x = literal.span().source_text().unwrap();
+            if x.len() == 1 && x.parse::<u8>().is_ok() {
+                eprintln!("got numeric literal {:?}", x);
+                Some(Ident::new(&format!("Digit{x}"), Span::call_site()))
+            } else {
+                let name = match x.as_str() {
+                    "'\\''" => Some("Apostrophe"),
+                    "'`'" => Some("Grave"),
+                    "'\\\\'" => Some("Backslash"),
+                    _ => todo!("literal char {x} {:?}", literal),
+                };
+                name.map(|x| Ident::new(x, Span::call_site()))
+            }
+        }
+        TokenTree::Punct(ref punct) => {
+            let name: Option<&str> = match punct.as_char() {
+                ';' => Some("Semicolon"),
+                ':' => {
+                    // TODO: `ctrl-:` Can't be entered on a US ANSI
+                    // keyboard only `shift-;` can. Make docs clear this
+                    // is the key and not the symbol?
+
+                    // add_shift = true;
+                    // Some("Semicolon")
+                    Some("Colon")
+                }
+                ',' => Some("Comma"),
+                '.' => Some("Period"),
+                '^' => Some("Caret"),
+                '=' => Some("Equals"),
+                '/' => Some("Slash"),
+                '-' => Some("Minus"),
+                '*' => Some("Asterisk"),
+                '+' => Some("Plus"),
+                '@' => Some("At"),
+                // _ => None
+                _ => todo!("punct {:?}", punct),
+            };
+            name.map(|n| Ident::new(n, punct.span()))
+        }
+        TokenTree::Ident(ref ident) => {
+            let label = ident.span().source_text().unwrap();
+            if label.len() == 1 {
+                let name: Option<Cow<'static, str>> = match label.chars().next().unwrap() {
+                    // x @ 'A'..='Z' => {
+                    x @ 'A'..='Z' => {
+                        // I'm not sure I like adding shift.
+                        // add_shift = true;
+                        // Some(x.to_string().into())
+                        Some(format!("Key{x}").into())
+                    }
+                    x @ 'a'..='z' => {
+                        abort!(x, "Use uppercase key names");
+                        // let s = x.to_ascii_uppercase().to_string();
+                        // Some(s.into())
+                    }
+                    '_' => Some("Underline".into()),
+                    // Identifiers can't start with a number.
+                    // x @ '0'..='9' => {
+                    //     let key = Ident::new(&format!("Key{x}"), ident.span());
+                    //     Some(quote! {key_code_path::#key })
+                    // },
+                    _ => todo!("ident {:?}", ident),
+                };
+                name.as_ref().map(|n| Ident::new(n, ident.span()))
+            } else {
+                Some(ident.clone())
+            }
+        }
+        _ => None,
+    }
+}
+
+fn get_key_code_bevy(tree: TokenTree) -> Option<Ident> {
     match tree {
         TokenTree::Literal(ref literal) => {
             let x = literal.span().source_text().unwrap();
@@ -214,7 +290,13 @@ fn partial_key(input: TokenStream) -> (TokenStream, TokenStream) {
 
     while let Some(tree) = i.next() {
         if i.peek().is_none() || (!is_dash(&tree) && !is_dash(i.peek().unwrap())) {
-            key_code = get_key_code(tree);
+            if cfg!(feature = "bevy") {
+                key_code = get_key_code_bevy(tree);
+            } else if cfg!(feature = "winit") {
+                key_code = get_key_code_winit(tree);
+            } else {
+                key_code = get_key_code_bevy(tree);
+            }
             break;
         } else {
             let replacement = match tree {
