7361b4a53460093bad9e1006be4a4030686989ad
taiga.watanabe <taiga.watanabe@va-sa-ra.co.jp>
2024-04-23 17:20:26 +0900

    doc: update documents

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,7 @@
 
 All notable changes to this project will be documented in this file.
 
-## [unreleased]
+## [0.4.0] - 2024-04-23
 
 ### Features
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
-#![doc(html_root_url = "https://docs.rs/bevy-input-sequence/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/bevy-input-sequence/0.4.0")]
 #![doc = include_str!("../README.md")]
 #![forbid(missing_docs)]
+
+pub use keyseq::{
+    bevy::{pkey as key, pkeyseq as keyseq},
+    Modifiers,
+};
+
+pub use chord::KeyChord;
+pub use plugin::InputSequencePlugin;
+pub use time_limit::TimeLimit;
 
 pub mod action;
 mod cache;
@@ -12,21 +21,13 @@ pub mod input_sequence;
 mod plugin;
 mod time_limit;
 
-pub use keyseq::{
-    bevy::{pkey as key, pkeyseq as keyseq},
-    Modifiers,
-};
-
 /// Convenient splat import
 pub mod prelude {
-    pub use super::{action, keyseq, InputSequencePlugin, Modifiers, TimeLimit};
+    pub use std::time::Duration;
+
     pub use crate::input_sequence::{ButtonSequence, InputSequence, KeySequence};
 
+    pub use super::{action, InputSequencePlugin, keyseq, Modifiers, TimeLimit};
     pub use super::cond_system::IntoCondSystem;
-    pub use std::time::Duration;
 }
 
-pub use time_limit::TimeLimit;
-
-pub use chord::KeyChord;
-pub use plugin::InputSequencePlugin;
