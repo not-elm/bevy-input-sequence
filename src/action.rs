bbda2e5a83972e876dcd74e77ed04b0a5a23956e
Shane Celis <shane.celis@gmail.com>
2024-11-23 07:09:22 -0500

    feature: Add trigger actions.

--- src/action.rs
+++ src/action.rs
@@ -1,5 +1,7 @@
 //! Common actions to do on key sequence matches
 use bevy::ecs::{
+    prelude::{Commands},
+    observer::TriggerTargets,
     event::{Event, EventWriter},
     system::In,
 };
@@ -22,6 +24,20 @@ use bevy::ecs::{
 pub fn send_event<E: Event + Clone>(event: E) -> impl FnMut(EventWriter<E>) {
     move |mut writer: EventWriter<E>| {
         writer.send(event.clone());
+    }
+}
+
+/// Trigger and event.
+pub fn trigger<E: Event + Clone>(event: E) -> impl FnMut(Commands) {
+    move |mut commands: Commands| {
+        commands.trigger(event.clone());
+    }
+}
+
+/// Trigger and event with targets.
+pub fn trigger_targets<E: Event + Clone, T: TriggerTargets + Clone>(event: E, targets: T) -> impl FnMut(Commands) {
+    move |mut commands: Commands| {
+        commands.trigger_targets(event.clone(), targets.clone());
     }
 }
 
